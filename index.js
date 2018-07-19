0fe236717d6ce411d9b4886ac9ffe87b537edf6e
huatao <huatao@huataodeMacBook-Pro.local>
2018-07-19 10:39:24 +0800

    修改编辑色值的问题

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ convStyle.install = function (Vue,options={}) {
         } catch (error) { 
              window.ISWX = false
         }
-        return(str + "").replace(/\s*(\d+)[\n\s\r]*(?:r?px)?(%+\s*)?;{0,}/g, function (match, $1,$2) {
+        return(str + "").replace(/\s*(\d+)[\n\s\r]?(r?px)?(%+)?;{0,}/g, function (match, $1,$2,$3) {
             if (ISWX) {
-                return !!$2?$1+'%;':(Math.round($1/750*winWid) + "px;");
+                // return !!$2?(Math.round($1/750) + "px;"):$3?($1+'%;'):$1+';';
+                return !!$2?(Math.round($1/750*winWid) + "px;"):$3?($1+'%;'):$1+';';
             }
-           return!!$2?$1+'%;':($1*(1/100)).toFixed(1) +`${obj.unit};`;
-        });
+            // !!$2?($1*(1/100)).toFixed(1) +`rem;`:$3?($1+'%;'):$1+';'
+           return !!$2?($1*(1/100)).toFixed(1) +`${obj.unit};`:$3?($1+'%;'):$1+';';
+        }).replace(/(;|\s)+/g,';');
     }
 }
 
