6dc0b61baf3f6e5ca44a434fac9283791bfd1e2e
huatao <841096772@qq.com>
2018-07-23 22:47:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ convStyle.install = function (Vue,options={}) {
         }
         return(str + "").replace(/\s*(\d+)[\n\s\r]?(r?px)?(%+)?;{0,}/g, function (match, $1,$2,$3) {
             if (ISWX) {
-                // return !!$2?(Math.round($1/750) + "px;"):$3?($1+'%;'):$1+';';
                 return !!$2?(Math.round($1/750*winWid) + "px;"):$3?($1+'%;'):$1+';';
             }
-            // !!$2?($1*(1/100)).toFixed(1) +`rem;`:$3?($1+'%;'):$1+';'
            return !!$2?($1*(1/100)).toFixed(1) +`${obj.unit};`:$3?($1+'%;'):$1+';';
         }).replace(/(;|\s)+/g,';');
     }
