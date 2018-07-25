e533353f323fb2d4b3105f3a8bb6216bf417e9cf
huatao <huatao@huataodeMacBook-Pro.local>
2018-07-25 15:18:04 +0800

    更新小树计算

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ convStyle.install = function (Vue,options={}) {
         unit: 'rem'
     },options)
     Vue.prototype.$convstyle = function(str) {
+        if(typeof str == 'object') {
+            str = JSON.stringify(str)
+        }
         let winWid;
         try {
             if (!!ISWX) {
@@ -13,14 +16,12 @@ convStyle.install = function (Vue,options={}) {
         } catch (error) { 
              window.ISWX = false
         }
-        return(str + "").replace(/\s*(\d+)[\n\s\r]?(r?px)?(%+)?;{0,}/g, function (match, $1,$2,$3) {
+        return(str).replace(/\s*(\d+[\.]?\d{0,})(r?px)?(%+)?/g, function (match, $1,$2,$3) {
             if (ISWX) {
-                // return !!$2?(Math.round($1/750) + "px;"):$3?($1+'%;'):$1+';';
-                return !!$2?(Math.round($1/750*winWid) + "px;"):$3?($1+'%;'):$1+';';
+                return !!$2?(Math.round($1/750*winWid) + "px"):$3?($1+'%'):$1;
             }
-            // !!$2?($1*(1/100)).toFixed(1) +`rem;`:$3?($1+'%;'):$1+';'
-           return !!$2?($1*(1/100)).toFixed(1) +`${obj.unit};`:$3?($1+'%;'):$1+';';
-        }).replace(/(;|\s)+/g,';');
+           return !!$2?($1*(1/100)).toFixed(1) +`${obj.unit}`:$3?($1+'%'):$1;
+        }).replace(/(\s+)|,/g,';').replace(/(\{|\})/g,'');
     }
 }
 
